35cabc1ad8687e06da84a5e8f1da0c3a6d9f5e48
EtienneDesticourt <etienne.desticourt@github.com>
2015-11-26 22:57:46 +0100

    Added CLI, fixed param order

--- exon.py
+++ exon.py
@@ -1,6 +1,8 @@
 import socket
 import time, json
 
+HOST = "localhost"
+PORT = 8878
 
 class Client(object):
     "Sends and receives message to and from the mess manager server."
@@ -15,11 +17,11 @@ class Client(object):
         self.socket = socket.socket()
         self.socket.connect((host, port))
     
-    def formatMessage(self, command, parameters={}):
+    def formatMessage(self, command, parameters=[]):
         "Format a command and its parameters into a string understandable by the server."
         message = command
         if parameters != {}: message += " "
-        formattedParameters = [key + "=\"" + parameters[key] + "\"" for key in parameters]
+        formattedParameters = [key + "=\"" + value + "\"" for key, value in parameters]
         message += "::".join(formattedParameters)
         message += "\n" #End character for server
         return message.encode('utf-8')
@@ -35,6 +37,7 @@ class Client(object):
             #Create mesage
             commandId, commandArgs = command(self, *args)
             message = self.formatMessage(commandId, commandArgs)
+            print(message)
             #Send message and wait for reply
             sent = self.socket.send(message)
             if not sent: raise socket.error("The server closed the socket unexpectedly.")
@@ -45,33 +48,67 @@ class Client(object):
     @executeCommand
     def getHelp(self):
         "Gets status information on the mess manager."
-        return ('help', {})
+        return ('help', [])
         
     @executeCommand
     def getObject(self, ID):
         "Gets an object from its ID."
-        return ('id', {'id':ID})
+        return ('id', [('id',ID)])
 
     @executeCommand
     def addNew(self, name, comments):
         "Adds a new object to the mess manager."
-        return ('add new', {'name': name, 'comments': comments})
+        return ('add new', [('name', name), ('comments', comments)])
 
     @executeCommand
     def addComment(self, ID, comments):
         "Adds a new comment to existing object with ID."
-        return ('add comment', {'id': ID, 'comments': comments})            
+        return ('add comment', [('id', ID), ('comments', comments)])         
 
     
-if __name__=='__main__':
-    HOST = "hemochro.me"
-    PORT = 8878
+if __name__ == '__main__':
+    import argparse, sys
 
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--add', nargs=3,
+                        help='Can either add new or add comment using respectively name, comment and id, comment')
+    parser.add_argument('--id')
+    parser.add_argument('--host')
+    parser.add_argument('--port', nargs=1)
+
+    args = parser.parse_args(sys.argv[1:])
+    if not args.add and not args.id:
+        parser.error("At least one command required. See -h for usage.")
+    if args.add and args.id:
+        parser.error("Too many commands. See -h for usage.")
+
+    host = args.host or HOST
+    port = args.port or PORT
+    
+    #Check whether host is IP, if not resolve it
     try:
-        ip = socket.gethostbyname(HOST)
-    except socket.gaierror:
-        print("Unable to resolve host.")
-        exit(1)
-        
-    C = Client(ip, PORT)
+        socket.inet_aton(host)
+    except socket.error:
+        try:
+            host = socket.gethostbyname(host)
+        except socket.gaierror:
+            print("Unable to resolve host.")
+            exit(1)
+
+    #Call command
+    C = Client(host, port)
+    if args.add:
+        if args.add[0] == "new":
+            print(C.addNew(args.add[1], args.add[2]))
+            exit(0)
+        elif args.add[0] == "comment":
+            print(C.addComment(args.add[1], args.add[2]))
+            exit(0)
+        else:
+            parser.error("Invalid argument for command add. See -h for usage help.")
+
+    if args.id:
+        if args.id:
+            print(C.getObject(args.id))
+            exit(0)
     
